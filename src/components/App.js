ddb3f56714061344e5748ac365f2edd16b9276de
NicMortelliti <npmortelliti@gmail.com>
2022-04-18 16:03:13 -0700

    Delete unused create-react files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
 import ItemList from "./ItemList";
 import NewItemForm from "./NewItemForm";
 
+const URL = "http://localhost:3000/checklist";
+
 function App() {
+  // Fetch GET checklist data
+  useEffect(() => {
+    fetch(URL)
+      .then(r => r.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
