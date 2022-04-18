d834bfacb9fb1237b2d1ef157ca31147ae3e18d4
NicMortelliti <npmortelliti@gmail.com>
2022-04-18 15:18:38 -0700

    Add react import. Remove unused imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 import NavBar from "./NavBar";
 import ItemList from "./ItemList";
 import NewItemForm from "./NewItemForm";
