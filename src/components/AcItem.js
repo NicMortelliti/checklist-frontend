1d87676c2ca3690ba8a4df59b8aeb661a1305c43
NicMortelliti <npmortelliti@gmail.com>
2022-04-19 12:48:10 -0700

    Add new item component for top-level aircraft selection menu.

---
+++ src/components/AcItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AcItem({ itemData }) {
+  return <h3>{itemData}</h3>;
+}
+
+export default AcItem;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Item from "./Item";
+import AcItem from "./AcItem";
 
 function ItemList({ currentAc, acArray, checklistItems }) {
   return (
@@ -7,7 +8,7 @@ function ItemList({ currentAc, acArray, checklistItems }) {
       {
         // If CurrentAC is not blank, list checklist items for selected aircraft
         currentAc === ""
-          ? acArray.map(ac => <Item key={ac} itemData={ac} />)
+          ? acArray.map(ac => <AcItem key={ac} itemData={ac} />)
           : checklistItems.map(item =>
               currentAc === item.tail ? ( // If checklist item tail number matches currentAC, render it.
                 <Item key={item.id} itemData={item} />
