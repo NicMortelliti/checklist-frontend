84bbaa3e0b8bc63e82b6cc3d0824ab8721d3508c
NicMortelliti <npmortelliti@gmail.com>
2022-04-19 09:59:30 -0700

    Populate NavBar with menu bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
 
-function NavBar() {
-  return(
-    <h3>NavBar Component Loaded</h3>
+function NavBar({ currentAC }) {
+  const [activePhase, setActivePhase] = useState("");
+
+  const phases = [
+    "Preflight",
+    "Taxi",
+    "Runup",
+    "Takeoff",
+    "Climb",
+    "Cruise",
+    "Descent",
+    "Landing",
+    "After Landing",
+    "Shutdown",
+  ];
+
+  return (
+    <Menu>
+      <Menu.Item name="🔙" onClick={() => console.log("Clicked back button")} />
+      <Menu.Item header>{currentAC}</Menu.Item>
+      {phases.map(phase => {
+        return (
+          <Menu.Item
+            key={phase}
+            name={phase}
+            active={activePhase === phase}
+            onClick={phase => setActivePhase(phase)}
+          />
+        );
+      })}
+    </Menu>
   );
 }
 
