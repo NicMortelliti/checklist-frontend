ff47ce0b2295984cece945be44b3f33cd4ebf536
NicMortelliti <npmortelliti@gmail.com>
2022-04-19 21:20:16 -0700

    Chnage button to card ui

--- src/components/AcItem.js
+++ src/components/AcItem.js
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
-import { Button } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 import { AcContext } from "../context/ac";
 
 function AcItem({ itemData }) {
   const { setAc } = useContext(AcContext);
 
   return (
-    <Button size="massive" fluid onClick={() => setAc(itemData)}>
-      {itemData}
-    </Button>
+    <Card raised fluid onClick={() => setAc(itemData)}>
+      <Card.Header textAlign="center">{itemData}</Card.Header>
+    </Card>
   );
 }
 
