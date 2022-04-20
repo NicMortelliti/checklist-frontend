29f1b7e7017c0f662b2fba26a391e55a6c07e9e6
NicMortelliti <npmortelliti@gmail.com>
2022-04-19 20:06:09 -0700

    Remove unused useState import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { Icon, Menu } from "semantic-ui-react";
 import { AcContext } from "../context/ac";
 
