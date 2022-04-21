a082303050bb30fd4abf3faac9512f48824de9f1
NicMortelliti <npmortelliti@gmail.com>
2022-04-21 15:27:29 -0700

    Add message component that signifies successful server post. No logic yet.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,27 +2,36 @@ import React, { useContext } from "react";
 import Item from "./Item";
 import AcItem from "./AcItem";
 import { AcContext } from "../context/ac";
-import { Segment } from "semantic-ui-react";
+import { Message, Segment, Transition } from "semantic-ui-react";
 
 function ItemList({ acArray, checklistItems, activePhase }) {
   const { ac } = useContext(AcContext);
   return (
-    <Segment attached="bottom">
-      {
-        // If CurrentAC is not blank, list checklist items for selected aircraft
-        ac === ""
-          ? acArray.map(aircraft => (
-              <AcItem key={aircraft.id} itemData={aircraft} />
-            ))
-          : checklistItems.map(item =>
-              ac === item.tail ? (
-                activePhase === item.phase ? (
-                  <Item key={item.id} itemData={item} />
+    <>
+      <Segment attached="bottom">
+        {
+          // If CurrentAC is not blank, list checklist items for selected aircraft
+          ac === ""
+            ? acArray.map(aircraft => (
+                <AcItem key={aircraft.id} itemData={aircraft} />
+              ))
+            : checklistItems.map(item =>
+                ac === item.tail ? (
+                  activePhase === item.phase ? (
+                    <Item key={item.id} itemData={item} />
+                  ) : null
                 ) : null
-              ) : null
-            )
-      }
-    </Segment>
+              )
+        }
+      </Segment>
+      <Transition animation={"jiggle"} duration={500} visible={true}>
+        <Message positive floating>
+          <Message.Header>
+            Successfully added new checklist item!
+          </Message.Header>
+        </Message>
+      </Transition>
+    </>
   );
 }
 
