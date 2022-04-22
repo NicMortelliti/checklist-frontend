4ae14561d4e7e1be3370edb9c032618f5eabd054
NicMortelliti <npmortelliti@gmail.com>
2022-04-21 20:06:40 -0700

    Move success message from ItemList to NewItemForm

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -7,31 +7,22 @@ import { Message, Segment, Transition } from "semantic-ui-react";
 function ItemList({ acArray, checklistItems, activePhase }) {
   const { ac } = useContext(AcContext);
   return (
-    <>
-      <Segment attached="bottom">
-        {
-          // If CurrentAC is not blank, list checklist items for selected aircraft
-          ac === ""
-            ? acArray.map(aircraft => (
-                <AcItem key={aircraft.id} itemData={aircraft} />
-              ))
-            : checklistItems.map(item =>
-                ac === item.tail ? (
-                  activePhase === item.phase ? (
-                    <Item key={item.id} itemData={item} />
-                  ) : null
+    <Segment attached="bottom">
+      {
+        // If CurrentAC is not blank, list checklist items for selected aircraft
+        ac === ""
+          ? acArray.map(aircraft => (
+              <AcItem key={aircraft.id} itemData={aircraft} />
+            ))
+          : checklistItems.map(item =>
+              ac === item.tail ? (
+                activePhase === item.phase ? (
+                  <Item key={item.id} itemData={item} />
                 ) : null
-              )
-        }
-      </Segment>
-      <Transition animation={"jiggle"} duration={500} visible={true}>
-        <Message positive floating>
-          <Message.Header>
-            Successfully added new checklist item!
-          </Message.Header>
-        </Message>
-      </Transition>
-    </>
+              ) : null
+            )
+      }
+    </Segment>
   );
 }
 
