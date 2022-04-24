d065f1c17c1c611b181bd6d429f577904568b840
NicMortelliti <npmortelliti@gmail.com>
2022-04-24 07:30:12 -0700

    Add 'add' button. Not yet functional

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,25 +1,37 @@
 import React, { useContext } from "react";
 import Item from "./Item";
+//import NewItemForm from "./NewItemForm";
+
 import { AcContext } from "../context/ac";
-import { Stack } from "@mui/material";
+import { Stack, Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 function ItemList({ checklistItems, activePhase }) {
   const { ac } = useContext(AcContext);
   return (
-    <Stack alignItems="center" spacing={2}>
-      {
-        // If CurrentAC is not blank, list checklist items for selected aircraft
-        ac !== ""
-          ? checklistItems.map(item =>
-              ac === item.tail ? (
-                activePhase === item.phase ? (
-                  <Item key={item.id} itemData={item} />
+    <>
+      <Stack alignItems="center" spacing={2}>
+        {
+          // If CurrentAC is not blank, list checklist items for selected aircraft
+          ac !== ""
+            ? checklistItems.map(item =>
+                ac === item.tail ? (
+                  activePhase === item.phase ? (
+                    <Item key={item.id} itemData={item} />
+                  ) : null
                 ) : null
-              ) : null
-            )
-          : null
-      }
-    </Stack>
+              )
+            : null
+        }
+      </Stack>
+      <Fab
+        color="primary"
+        aria-label="add"
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+      >
+        <AddIcon />
+      </Fab>
+    </>
   );
 }
 
