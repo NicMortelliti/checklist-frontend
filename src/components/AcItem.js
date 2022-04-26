0a8b962c3ca96cab72619d59da48a854d2943bba
NicMortelliti <npmortelliti@gmail.com>
2022-04-26 16:06:14 -0700

    Put box around ac item content to control margin

--- src/components/AcItem.js
+++ src/components/AcItem.js
@@ -1,27 +1,29 @@
 import React, { useContext } from "react";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { AcContext } from "../context/ac";
 
 function AcItem({ itemData }) {
   const { setAc } = useContext(AcContext);
 
   return (
-    <Card sx={{ maxWidth: 345 }} onClick={() => setAc(itemData.tail)}>
-      <CardMedia
-        component="img"
-        height="250"
-        image={itemData.image}
-        alt={itemData.tail}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {itemData.tail}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {itemData.model}
-        </Typography>
-      </CardContent>
-    </Card>
+    <Box m={2}>
+      <Card sx={{ maxWidth: 345 }} onClick={() => setAc(itemData.tail)}>
+        <CardMedia
+          component="img"
+          height="250"
+          image={itemData.image}
+          alt={itemData.tail}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {itemData.tail}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {itemData.model}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
 
