2058061c3d8da2c9d30516e4d1f7e06b07becced
NicMortelliti <npmortelliti@gmail.com>
2022-04-25 20:50:47 -0700

    Add logic to component. Set call state on field change.

--- src/components/NewItemDescription.js
+++ src/components/NewItemDescription.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React from "react";
+import { TextField } from "@mui/material";
 
-function NewItemDescription() {
-  return null
+function NewItemDescription({ call, setCall }) {
+  return (
+    <TextField
+      id="description-text"
+      label="Call"
+      variant="standard"
+      onChange={e => setCall(e.target.value)}
+    />
+  );
 }
 
-export default NewItemDescription
+export default NewItemDescription;
