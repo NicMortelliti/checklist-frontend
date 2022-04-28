cb519cb017a777e77e86cb16ea9c1604b054314f
NicMortelliti <npmortelliti@gmail.com>
2022-04-28 14:33:54 -0700

    Remove box component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { AppBar, Box, Toolbar, Typography, IconButton } from "@mui/material/";
+import { AppBar, Toolbar, Typography, IconButton } from "@mui/material/";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { AcContext } from "../context/ac";
 import PhaseSelect from "./PhaseSelect";
@@ -8,26 +8,24 @@ function NavBar() {
   const { ac, setAc } = useContext(AcContext);
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <AppBar position="fixed" open="true">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="back"
-            sx={{ mr: 2 }}
-            onClick={() => setAc("")}
-          >
-            <ChevronLeftIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {ac}
-          </Typography>
-          <PhaseSelect />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="fixed" open="true">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="back"
+          sx={{ mr: 2 }}
+          onClick={() => setAc("")}
+        >
+          <ChevronLeftIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {ac}
+        </Typography>
+        <PhaseSelect />
+      </Toolbar>
+    </AppBar>
   );
 }
 export default NavBar;
