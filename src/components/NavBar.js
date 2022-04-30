fcbfea6e64173844a483115dc6cdd419ac84f6ac
NicMortelliti <npmortelliti@gmail.com>
2022-04-29 18:01:26 -0700

    Simplify app. Replace phase select component with nav bar buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,18 @@
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Typography, IconButton } from "@mui/material/";
+import {
+  AppBar,
+  Button,
+  Toolbar,
+  Typography,
+  IconButton,
+} from "@mui/material/";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { AcContext } from "../context/ac";
-import PhaseSelect from "./PhaseSelect";
+import { PhaseContext } from "../context/phase";
 
 function NavBar() {
   const { ac, setAc } = useContext(AcContext);
+  const { currentPhase, setCurrentPhase } = useContext(PhaseContext);
 
   return (
     <AppBar position="fixed" open={true}>
@@ -23,7 +30,19 @@ function NavBar() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           {ac}
         </Typography>
-        <PhaseSelect />
+        <Button color="inherit" onClick={() => setCurrentPhase("Preflight")}>
+          Preflight
+        </Button>
+        <Button color="inherit" onClick={() => setCurrentPhase("Taxi")}>
+          Taxi
+        </Button>
+        <Button
+          color="error"
+          onClick={() => setCurrentPhase("Emergency")}
+          variant="contained"
+        >
+          EMERGENCY
+        </Button>
       </Toolbar>
     </AppBar>
   );
