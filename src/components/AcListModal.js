4093f8f06d938174c49d5a9c9c53bc96ce4c6d5b
NicMortelliti <npmortelliti@gmail.com>
2022-04-30 09:42:36 -0700

    Convert ac modal to full page to utilize routing to be added later.

--- src/components/AcListModal.js
+++ src/components/AcListModal.js
@@ -1,26 +1,18 @@
 import React, { useContext } from "react";
 import AcItem from "./AcItem";
 import { AcContext } from "../context/ac";
-import { Dialog, DialogContent, DialogTitle, Slide } from "@mui/material";
+import { Paper } from "@mui/material";
 
 function AcListModal({ acArray }) {
   const { ac } = useContext(AcContext);
 
-  // Material UI transition effect
-  const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-  });
-
-  return !ac ? (
-    <Dialog open={!ac} TransitionComponent={Transition} scroll="paper">
-      <DialogTitle>Select an aircraft</DialogTitle>
-      <DialogContent dividers={true}>
-        {acArray.map(aircraft => (
-          <AcItem key={aircraft.id} itemData={aircraft} />
-        ))}
-      </DialogContent>
-    </Dialog>
-  ) : null;
+  return (
+    <Paper sx={{ pt: "50px" }}>
+      {acArray.map(aircraft => (
+        <AcItem key={aircraft.id} itemData={aircraft} />
+      ))}
+    </Paper>
+  );
 }
 
 export default AcListModal;
