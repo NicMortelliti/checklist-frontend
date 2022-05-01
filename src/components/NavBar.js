1413c7a195ac1c1f8cd1df1dce431fdaadfddc14
NicMortelliti <npmortelliti@gmail.com>
2022-05-01 11:43:04 -0700

    Add routing to nav bar back button. Set ac to emptry string with back button clicked.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AppBar, Button, Toolbar, Typography } from "@mui/material/";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { AcContext } from "../context/ac";
@@ -11,15 +11,19 @@ function NavBar() {
 
   const phases = ["Preflight", "Taxi", "Emergency"];
 
-  const match = useRouteMatch();
-
   // Render nav bar message
   const renderMsg = <Typography variant="h6">Select an aircraft...</Typography>;
 
   // Render nav bar buttons
   const renderBtns = (
     <div>
-      <Button color="inherit" startIcon={<ChevronLeftIcon />}>
+      <Button
+        component={Link}
+        to={`/`}
+        color="inherit"
+        startIcon={<ChevronLeftIcon />}
+        onClick={() => setAc("")}
+      >
         {ac}
       </Button>
       {phases.map(phase => (
