beb5a5375a54a57a5038f248eee074a39276b1a0
NicMortelliti <npmortelliti@gmail.com>
2022-05-04 16:09:52 -0700

    Reduce new item select components to singular component

---
+++ src/components/FormSelect.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+function NewItemResponse({ array, handleChange, label, controlValue }) {
+  return (
+    <FormControl sx={{ m: 1, width: 250 }}>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        value={controlValue}
+        label={label}
+        onChange={e => handleChange(e.target.value)}
+      >
+        {array.map(arrayItem => (
+          <MenuItem key={arrayItem} value={arrayItem}>
+            {arrayItem}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
+
+export default NewItemResponse;
--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -3,21 +3,40 @@ import { Button, FormControl, FormGroup, Paper } from "@mui/material";
 import { AcContext } from "../context/ac";
 import NewItemPhase from "./NewItemPhase";
 import NewItemDescription from "./NewItemDescription";
-import NewItemResponse from "./NewItemResponse";
+import FormSelect from "./FormSelect";
 
 function NewItemForm({ url, acArray, setAcArray }) {
   const { ac } = useContext(AcContext);
-  const [phase, setPhase] = useState("");
-  const [call, setCall] = useState("");
-  const [response, setResponse] = useState("");
+  const [controlPhase, setControlPhase] = useState("");
+  const [controlCall, setControlCall] = useState("");
+  const [controlResponse, setControlResponse] = useState("");
+
+  // Array of responses
+  const responseOptions = [
+    "70 KTS",
+    "ACCESSIBLE",
+    "CHECK",
+    "COMPLETE",
+    "EXTEND",
+  ];
+
+  // Array of phases
+  const phaseOptions = [
+    "Preflight",
+    "Taxi",
+    "Takeoff",
+    "Cruise",
+    "Landing",
+    "Emergency",
+  ];
 
   function handleSubmit(e) {
     e.preventDefault();
     const itemData = {
       tail: ac,
-      phase: phase,
-      description: call,
-      response: response,
+      phase: controlPhase,
+      description: controlCall,
+      response: controlResponse,
     };
     fetch(`${url}/checklist`, {
       method: "POST",
@@ -33,18 +52,28 @@ function NewItemForm({ url, acArray, setAcArray }) {
   }
 
   function updateStates() {
-    setPhase("");
-    setCall("");
-    setResponse("");
+    setControlPhase("");
+    setControlCall("");
+    setControlResponse("");
   }
 
   return (
     <Paper sx={{ pt: "50px" }}>
       <FormGroup>
         <FormControl required>
-          <NewItemPhase phase={phase} setPhase={setPhase} />
-          <NewItemDescription call={call} setCall={setCall} />
-          <NewItemResponse response={response} setResponse={setResponse} />
+          <FormSelect
+            array={phaseOptions}
+            handleChange={setControlPhase}
+            label={"Phase of flight*"}
+            controlValue={controlPhase}
+          />
+          <NewItemDescription call={controlCall} setCall={setControlCall} />
+          <FormSelect
+            array={responseOptions}
+            handleChange={setControlResponse}
+            label={"Response*"}
+            controlValue={controlResponse}
+          />
           <Button
             variant="contained"
             color="primary"
