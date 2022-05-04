df67260c8f14447ef6c4e5b26d0f0d2a4d3939c0
NicMortelliti <npmortelliti@gmail.com>
2022-05-04 15:20:45 -0700

    Fix when Emergency button in the nav bar gets displayed.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AppBar, Button, Toolbar, Typography } from "@mui/material/";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material/";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { AcContext } from "../context/ac";
 import { PhaseContext } from "../context/phase";
 
 function NavBar() {
   const { ac, setAc } = useContext(AcContext);
-  const { setCurrentPhase } = useContext(PhaseContext);
+  const { currentPhase, setCurrentPhase } = useContext(PhaseContext);
 
   // Render nav bar message
   const renderMsg = <Typography variant="h6">Select an aircraft...</Typography>;
@@ -24,22 +24,26 @@ function NavBar() {
       >
         {ac}
       </Button>
-      <Button
-        component={Link}
-        to={`/${ac}/Emergency`}
-        color="error"
-        variant="contained"
-        onClick={() => setCurrentPhase("Emergency")}
-      >
-        EMERGENCY
-      </Button>
+      {currentPhase ? (
+        <Button
+          component={Link}
+          to={`/${ac}/Emergency`}
+          color="error"
+          variant="contained"
+          onClick={() => setCurrentPhase("Emergency")}
+        >
+          EMERGENCY
+        </Button>
+      ) : null}
     </div>
   );
 
   return (
-    <AppBar position="fixed" open={true}>
-      <Toolbar>{ac ? renderBtns : renderMsg}</Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed">
+        <Toolbar>{ac ? renderBtns : renderMsg}</Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 export default NavBar;
