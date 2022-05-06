ed3edbf19b4ec840bd856ceac6fb9d67423cf81f
NicMortelliti <npmortelliti@gmail.com>
2022-05-06 11:44:14 -0700

    Simplify new form implementation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,83 +1,82 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import {
-  AppBar,
-  Box,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography,
+    AppBar,
+    Box,
+    Button,
+    IconButton,
+    Toolbar,
+    Typography,
 } from "@mui/material/";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import { AcContext } from "../context/ac";
 import { PhaseContext } from "../context/phase";
 
 function NavBar() {
-  const { ac, setAc } = useContext(AcContext);
-  const { currentPhase, setCurrentPhase } = useContext(PhaseContext);
+    const { ac, setAc } = useContext(AcContext);
+    const { currentPhase, setCurrentPhase } = useContext(PhaseContext);
 
-  // Render nav bar message
-  const renderMsg = <Typography variant="h6">Select an aircraft...</Typography>;
+    // Render nav bar message
+    const renderMsg = (
+        <Typography variant="h6">Select an aircraft...</Typography>
+    );
 
-  // Render nav bar buttons
-  const renderBtns = (
-    <>
-      {/*
+    // Render nav bar buttons
+    const renderBtns = (
+        <>
+            {/*
       The Box component with its 'display' and 'flexGrow'
       properties pushes components rendered after it (e.g.
       Emergency btn) to be pushed to the right of the display.
       */}
-      <Box display="flex" flexGrow={1}>
-        <IconButton
-          component={Link}
-          to={`/`}
-          size="large"
-          edge="start"
-          color="inherit"
-          sx={{ mr: 2 }}
-          onClick={() => setAc("")}
-        >
-          <ArrowBackIosNewIcon />
-        </IconButton>
-        <Typography
-          margin="auto"
-          variant="h5"
-          component="div"
-          sx={{ flexGrow: 1 }}
-        >
-          {ac}
-        </Typography>
-      </Box>
+            <Box display="flex" flexGrow={1}>
+                <IconButton
+                    component={Link}
+                    to={`/`}
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    sx={{ mr: 2 }}
+                    onClick={() => setAc("")}>
+                    <ArrowBackIosNewIcon />
+                </IconButton>
+                <Typography
+                    margin="auto"
+                    variant="h5"
+                    component="div"
+                    sx={{ flexGrow: 1 }}>
+                    {ac}
+                </Typography>
+            </Box>
 
-      {/*
+            {/*
       If a flight phase has been selected, then the app
       is displaying the checklist items. In this case, we want
       to show an Emergency button in the nav bar that is easily
       accessible.
       */}
-      {currentPhase ? (
-        <Button
-          component={Link}
-          to={`/${ac}/Emergency`}
-          color="error"
-          variant="contained"
-          onClick={() => setCurrentPhase("Emergency")}
-        >
-          EMERGENCY
-        </Button>
-      ) : null}
-    </>
-  );
+            {currentPhase ? (
+                <Button
+                    component={Link}
+                    to={`/${ac}/Emergency`}
+                    color="error"
+                    variant="contained"
+                    onClick={() => setCurrentPhase("Emergency")}>
+                    EMERGENCY
+                </Button>
+            ) : null}
+        </>
+    );
 
-  return (
-    <AppBar position="fixed">
-      {/*
+    return (
+        <AppBar position="fixed">
+            {/*
       If an aircraft hasn't been selected yet, display a message
       in the nav bar instructing the user to select an aircraft.
       Otherwise, display navigation buttons.
       */}
-      <Toolbar>{ac ? renderBtns : renderMsg}</Toolbar>
-    </AppBar>
-  );
+            <Toolbar>{ac ? renderBtns : renderMsg}</Toolbar>
+        </AppBar>
+    );
 }
 export default NavBar;
--- src/components/NewItemForm.js
+++ src/components/NewItemForm.js
@@ -4,37 +4,41 @@ import {
   Container,
   FormControl,
   FormGroup,
-  Grid,
-  Paper,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 import { AcContext } from "../context/ac";
-import NewItemDescription from "./NewItemDescription";
-import FormSelect from "./FormSelect";
+import { PhaseContext } from "../context/phase";
+
+// Array of responses
+const RESPONSES = ["70 KTS", "ACCESSIBLE", "CHECK", "COMPLETE", "EXTEND"];
 
 function NewItemForm({ url, listItems, acArray, setAcArray }) {
   const { ac } = useContext(AcContext);
-  const [controlPhase, setControlPhase] = useState("");
-  const [controlCall, setControlCall] = useState("");
-  const [controlResponse, setControlResponse] = useState("");
+  const { currentPhase } = useContext(PhaseContext);
+  const [formData, setFormData] = useState({
+    call: "",
+    response: "",
+  });
 
-  // Array of responses
-  const responseOptions = [
-    "70 KTS",
-    "ACCESSIBLE",
-    "CHECK",
-    "COMPLETE",
-    "EXTEND",
-  ];
-
+  // Post new form data to backend
   function handleSubmit(e) {
+    // Prevent reloading page
     e.preventDefault();
+
+    // Put information into single variable
     const itemData = {
       tail: ac,
-      phase: controlPhase,
-      description: controlCall,
-      response: controlResponse,
+      phase: currentPhase,
+      description: formData.call,
+      response: formData.response,
     };
+
+    // Fetch POST data to backend
     fetch(`${url}/checklist`, {
       method: "POST",
       headers: {
@@ -42,78 +46,83 @@ function NewItemForm({ url, listItems, acArray, setAcArray }) {
       },
       body: JSON.stringify(itemData),
     })
-      .then(r => r.json())
-      .then(r => console.log("Success:", JSON.stringify(r)))
-      .then(newItem => setAcArray([...acArray, newItem]))
+      .then((r) => r.json())
+      .then((r) => console.log("Success:", JSON.stringify(r)))
+      .then((newItem) => setAcArray([...acArray, newItem]))
       .then(updateStates);
   }
 
+  // Clear controlled form data
   function updateStates() {
-    setControlPhase("");
-    setControlCall("");
-    setControlResponse("");
+    setFormData({
+      call: "",
+      response: "",
+    });
   }
 
   return (
+    // Constrain width of container with maxWidth property
+    // in case page is displayed on desktop
     <Container component="main" maxWidth="sm">
-      <Paper variant="outlined" sx={{ my: 2, p: 2 }}>
-        <FormGroup>
-          <FormControl>
-            <Grid
-              container
-              justifyContent="center"
-              alignItems="center"
-              spacing={3}
-            >
-              <Grid item>
-                <Typography variant="h5" color="inherit" align="center">
-                  New Checklist Item
-                </Typography>
-                <Typography
-                  variant="subtitle2"
-                  color="inherit"
-                  align="center"
-                  gutterBottom
-                >
-                  {ac}
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <FormSelect
-                  array={listItems}
-                  handleChange={setControlPhase}
-                  label={"Phase of flight*"}
-                  controlValue={controlPhase}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <NewItemDescription
-                  call={controlCall}
-                  setCall={setControlCall}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormSelect
-                  array={responseOptions}
-                  handleChange={setControlResponse}
-                  label={"Response*"}
-                  controlValue={controlResponse}
-                />
-              </Grid>
-              <Grid item>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  type="submit"
-                  onClick={e => handleSubmit(e)}
-                >
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
-          </FormControl>
-        </FormGroup>
-      </Paper>
+      <FormGroup>
+        <FormControl>
+          {/* Header */}
+          <Typography variant="h5" color="inherit" align="center">
+            New Checklist Item
+          </Typography>
+
+          {/* Subheader */}
+          <Typography
+            variant="subtitle2"
+            color="inherit"
+            align="center"
+            gutterBottom>
+            {ac} - {currentPhase}
+          </Typography>
+
+          {/* Checklist item call text entry */}
+          <TextField
+            required
+            sx={{ mb: 1, width: 1 }}
+            value={formData.call}
+            label="Call (e.g. 'Parking Brake')"
+            variant="outlined"
+            onChange={(e) => setFormData(e.target.value)}
+          />
+
+          {/* Checklist item response selection */}
+          <InputLabel>"Response"</InputLabel>
+          <Select
+            required
+            sx={{ width: 1 }}
+            value={formData.response}
+            label={"Response"}
+            onChange={(e) => setFormData(e.target.value)}>
+            {RESPONSES.map((arrayItem) => (
+              <MenuItem key={arrayItem} value={arrayItem}>
+                {arrayItem}
+              </MenuItem>
+            ))}
+          </Select>
+
+          {/* Submit button */}
+          <Button
+            // Button position calculation:
+            //  screen width - button width
+            //        = white space / left side and right side (2)
+            //                = left starting position
+            //  100% - 30% = 70% / 2 = 35%
+            sx={{ left: "35%", width: "30%" }}
+            variant="contained"
+            color="primary"
+            type="submit"
+            component={Link}
+            to={`/${ac}/${currentPhase}`}
+            onClick={(e) => handleSubmit(e)}>
+            Submit
+          </Button>
+        </FormControl>
+      </FormGroup>
     </Container>
   );
 }
