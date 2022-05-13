8d76679d174caf9526ebdd9effc08ffaf51c7968
NicMortelliti <npmortelliti@gmail.com>
2022-05-13 13:25:56 -0700

    Change color of button to red if current phase is Emergency

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { AcContext } from "../context/ac";
+import { PhaseContext } from "../context/phase";
 
 function AddBtn() {
   const { currentAc } = useContext(AcContext);
+  const { currentPhase } = useContext(PhaseContext);
 
   return (
     <Fab
-      color="primary"
-      aria-label="add"
+      color={currentPhase === "Emergency" ? "error" : "primary"}
       sx={{ position: "fixed", bottom: 15, right: 15 }}
       component={Link}
       to={`/${currentAc}/newitem`}>
