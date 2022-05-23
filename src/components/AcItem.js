c47e52bb05ff2263d0a91d00cb804ec83fd12d07
NicMortelliti <npmortelliti@gmail.com>
2022-05-22 21:06:12 -0700

    MVP - Display a skeleton component.

--- src/components/AcItem.js
+++ src/components/AcItem.js
@@ -11,54 +11,50 @@ import {
 } from "@mui/material";
 import { AcContext } from "../context/ac";
 
-function AcItem({ data: { tail, model, image } }) {
+function AcItem({ data, data: { tail, model, image } }) {
   const { setCurrentAc } = useContext(AcContext);
 
-  const renderCard = () => {
-    return (
-      <Card
-        onClick={() => setCurrentAc(tail)}
-        component={Link}
-        to={`/${tail}`}
-        raised
-        sx={{
-          mb: 2,
-          display: "flex",
-          flexDirection: "column",
-        }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="200"
-            image={image}
-            alt={tail}
-            sx={{ display: "flex" }}
-          />
-          <CardContent>
-            <Grid
-              container
-              spacing={0}
-              direction="column"
-              alignItems="center"
-              justify="center">
-              <Typography variant="h5" component="h2">
-                {tail}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {model}
-              </Typography>
-            </Grid>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    );
-  };
+  const renderCard = (
+    <Card
+      onClick={() => setCurrentAc(tail)}
+      component={Link}
+      to={`/${tail}`}
+      raised
+      sx={{
+        mb: 2,
+        display: "flex",
+        flexDirection: "column",
+      }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="200"
+          image={image}
+          alt={tail}
+          sx={{ display: "flex" }}
+        />
+        <CardContent>
+          <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center">
+            <Typography variant="h5" component="h2">
+              {tail}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {model}
+            </Typography>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 
-  const renderSkeleton = () => {
-    return <Skeleton variant="rectangular" height={200} />;
-  };
+  const renderSkeleton = <Skeleton variant="rectangular" height={200} />;
 
-  return <>{image ? renderCard : renderSkeleton}</>;
+  return <>{renderSkeleton}</>;
 }
 
 export default AcItem;
